11d1193647dfc92b44f69767f35a69c482d6853b
Francois Laberge <flaberge@brightcove.com>
2015-02-22 13:11:13 -0500

    minor edit

--- examples/npm.js
+++ examples/npm.js
@@ -5,11 +5,7 @@
 var arbitrary = require('../arbitrary.js');
 
 console.log(arbitrary.float());
-
 console.log(arbitrary.integer());
-
 console.log(arbitrary.shuffle([1, 2, 3, 4]));
-
 console.log(arbitrary.choice([1, 2, 3, 4]));
-
 console.log(arbitrary.sample([1, 2, 3, 4], 2));
