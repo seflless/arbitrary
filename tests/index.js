638a1225588277467d9230f8af59142bcd975117
Francois Laberge <francois@jibo.com>
2016-08-07 13:44:21 -0400

    Added module compatibility test

--- tests/index.js
+++ tests/index.js
@@ -2,6 +2,10 @@ import assert from 'assert';
 import arbitrary from '../src/index';
 
 describe('arbitrary', function() {
+    describe('should be commonjs compatible', function () {
+        const arbitraryRequired = require('../src/index');
+        assert(arbitraryRequired === arbitrary, `ES6 module and commonjs module should have been equivalent. es6=${arbitrary}, commonjs=${arbitraryRequired}`);
+    });
     describe('Generator', function () {
         describe('constructor()', function () {
             it('should use provided seed', function () {
